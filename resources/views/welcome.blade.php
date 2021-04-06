c48aa5724731f45f8f999faaeb4cdf3edea28c1f
ej84 <ej84@njit.edu>
2021-04-06 10:09:13 -0400

    task: tested for some files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -25,7 +26,7 @@
     @if (Route::has('login'))
         <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             @auth
-                <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
+                <a href="{{ url('/posts') }}" class="text-sm text-gray-700 underline">Main</a>
             @else
                 <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
